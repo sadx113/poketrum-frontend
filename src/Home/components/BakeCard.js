55c933f5ce9a3f1e67febef6b5b2221a0a3c4c94
xOvADOS <95148195+sadx113@users.noreply.github.com>
2023-04-14 19:47:43 -0400

    Update BakeCard.js

--- src/Home/components/BakeCard.js
+++ src/Home/components/BakeCard.js
@@ -286,7 +286,7 @@ export default function BakeCard() {
               disabled={wrongNetwork || !address || +bakeBUSD === 0 || loading || +walletBalance.approved === 0}
               onClick={bake}
             >
-              BuyCoins
+              Buy Coins
             </Button>
           </Box>
         </Box>
